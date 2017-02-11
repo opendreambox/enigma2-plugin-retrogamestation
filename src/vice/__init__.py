829e8d4ce82c44aa0001056dc4184435b83a9929
Stephan Reichholf <reichi@opendreambox.org>
2017-02-11 17:12:16 +0100

    fixup vice

--- src/vice/__init__.py
+++ src/vice/__init__.py
@@ -11,7 +11,7 @@ class Vice(object):
 	name = _("Vice")
 	description = _("Commodore C64")
 	location = config.plugins.vice.romlocation
-	pattern = "^.*\.(d64|D64|t64|T64|crt|CRT|P00|p00|g64|G64|p64|P64|d67|D67|d71|D71|d81|D81|d80|D80|d82|D82|d1m|D1M|d2m|D2M|d4,|D4M)"
+	pattern = "^.*\.(d64|D64|t64|T64|crt|CRT|P00|p00|g64|G64|p64|P64|d67|D67|d71|D71|d81|D81|d80|D80|d82|D82|d1m|D1M|d2m|D2M|d4,|D4M|tap|TAP)"
 	cmd = "/usr/bin/vice-start"
 	icon = "vice/vice.png"
 
